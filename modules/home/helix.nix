0216655c583b3833d885a285b9da913e0217c361
AdamGuzman <adamguzman33@gmail.com>
2025-12-05 10:20:03 -0800

    keymaps

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -24,12 +24,36 @@
         indent-guides.render = true;
         soft-wrap.enable = true;
       };
+      keys = {
+        normal ={
+          space = {
+            e = [
+              ":sh rm -f /tmp/unique-file-h21a434",
+              ":insert-output yazi '%{buffer_name}' --chooser-file=/tmp/unique-file-h21a434",
+              ":insert-output echo \"x1b[?1049h\" > /dev/tty",
+              ":open %sh{cat /tmp/unique-file-h21a434}",
+              ":redraw",
+            ];
+            l = {
+              g = [
+                ":write-all",
+                ":insert-output lazygit >/dev/tty",
+                ":redraw",
+                ":reload-all"
+              ];
+            };
+          };
+        };
+      };
     };
     languages = {
       language-server = {
         typescript-language-server = with pkgs.nodePackages; {
           command = "${typescript-language-server}/bin/typescript-language-server";
           args = [ "--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib" ];
+        };
+        nil = with pkgs; {
+          command = "${nil}";
         };
       };
       language = [{
