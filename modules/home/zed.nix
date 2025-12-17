3c3f05d84fc2caa86590053dadca8ec8ecc5e934
AdamGuzman <adamguzman33@gmail.com>
2025-12-17 08:31:09 -0800

    zed e

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -15,19 +15,63 @@
       "Python LSP"
       "SQL"
       ];
-    vim_mode = true;
-
-    load_direnv = "shell_hook";
-    base_keymap = "VSCode";
-
-    theme = {
-      mode = "system";
-      light = "One Light";
-      dark = "One Dark";
+    userSettings = {
+      node = {
+      path = lib.getExe pkgs.nodejs;
+      npm_path = lib.getExe' pkgs.nodejs "npm";
     };
 
-    show_whitespaces = "all";
-    ui_font_size = 16;
-    buffer_font_size = 16;
+    hour_format = "hour24";
+    auto_update = false;
+
+    terminal = {
+      alternate_scroll = "off";
+      blinking = "off";
+      copy_on_select = false;
+      dock = "bottom";
+      detect_venv = {
+        on = {
+          directories = [ ".env" "env" ".venv" "venv" ];
+          activate_script = "default";
+        };
+      };
+      env = {
+        TERM = "alacritty";
+      };
+      font_family = "FiraCode Nerd Font";
+      font_features = null;
+      font_size = null;
+      line_height = "comfortable";
+      option_as_meta = false;
+      button = false;
+      shell = "system";
+      toolbar = {
+        title = true;
+      };
+      working_directory = "current_project_directory";
+    };
+
+    lsp = {
+      nix = {
+        binary = {
+          path_lookup = true;
+        };
+      };
+
+      vim_mode = true;
+
+      load_direnv = "shell_hook";
+      base_keymap = "VSCode";
+
+      theme = {
+        mode = "system";
+        light = "One Light";
+        dark = "One Dark";
+      };
+
+      show_whitespaces = "all";
+      ui_font_size = 16;
+      buffer_font_size = 16;
+    };
   };
 }
